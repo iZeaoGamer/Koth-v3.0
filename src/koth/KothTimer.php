656fae70d713a0ddc628c0192c7aed06f87d07b0
iZeaoGamer <jameshayward14@icloud.com>
2020-02-04 18:04:38 +0000

    Fixed #8
    This bug was if you had discord-support disabled in config.yml, errors such as sendToDiscord being null would occur.
    This is because discord won't recognize the discord support option when this is turned off. Should be fixed now.

--- src/koth/KothTimer.php
+++ src/koth/KothTimer.php
@@ -47,6 +47,7 @@ class KothTimer extends Task
 
        if($this->time === 300 || $this->time === 600 || $this->time === 1800 || $this->time === 3600){
 
+           if($this->plugin->get("discord-support"){
            $this->plugin->discord->sendToDiscord("**KOTH EVENT**\nKoth event is starting in " . gmdate("H:i:s", $this->time) . " on OP Factions.",
 
            $this->plugin->msg->get("webhook-url"),
@@ -60,14 +61,14 @@ class KothTimer extends Task
    
 
        if ($this->time == 30 || $this->time == 15 || $this->time < 6){
-
+if($this->plugin->get("discord-support"){
            
            $this->plugin->discord->sendToDiscord("**KOTH EVENT**\nKoth timer is starting in " . gmdate("i:s", $this->time) . " on OP Factions.",
 
            $this->plugin->msg->get("webhook-url"),
 
             $this->plugin->msg->get("bot-displayname"));
-
+}
             
        $this->plugin->getServer()->broadcastMessage(TextFormat::colorize("&6Koth event timer starts in &b" . gmdate("i:s", $this->time)));
 
@@ -78,14 +79,14 @@ class KothTimer extends Task
         if ($this->time < 1){
 
             $this->arena->preStart();
-
+if($this->plugin->get("discord-support"){
            
             $this->plugin->discord->sendToDiscord("**KOTH EVENT**\nKoth game is about to start! Join a koth game by going on OP Factions, and type /koth join\nIP: play.zectorpe.ml Port: 19132",
 
             $this->plugin->msg->get("webhook-url"),
 
             $this->plugin->msg->get("bot-displayname"));
-
+}
             $this->plugin->getServer()->broadcastMessage(TextFormat::colorize("&6KOTH Event is starting.. Type: &b/koth join &6to join the koth event!"));
 
       
