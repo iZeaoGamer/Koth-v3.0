92ab0f46b869de44d7fd67e6180e63fcdf316dbd
iZeaoGamer <jameshayward14@icloud.com>
2020-03-30 10:15:15 +0100

    Reformatting
    This build reformats KothPlayer::class code

--- src/koth/KothPlayer.php
+++ src/koth/KothPlayer.php
@@ -4,9 +4,11 @@ namespace koth;
 use pocketmine\Player;
 
 class KothPlayer extends Player{
+	
 public $kothingame = false;
-	public function isInGame(): bool{
-			return $this->kothingame;
+	
+    public function isInGame(): bool{
+	return $this->kothingame;
     }
     public function setInGame(bool $kothingame){
         $this->kothingame = $kothingame;
