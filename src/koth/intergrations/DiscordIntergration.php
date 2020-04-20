ec59c13be5c1bedb3b6b5226855db405a1003867
iZeaoGamer <jameshayward14@icloud.com>
2020-04-20 21:52:46 +0100

    Empty space fix + Reformatting
    This commit fixes an issue, where there would be a blank space within the code.

--- src/koth/intergrations/DiscordIntergration.php
+++ src/koth/intergrations/DiscordIntergration.php
@@ -5,13 +5,12 @@ use CortexPE\DiscordWebhookAPI\Message;
 
 class DiscordIntergration {
 	
-	 public function sendToDiscord(string $message, string $chat, string $username, $avatar = null){
+ public function sendToDiscord(string $message, string $chat, string $username, $avatar = null){
         $webHook = new Webhook($chat);
         $msg = new Message();
         $msg->setUsername($username);
         if($avatar !== null){
-            $msg->setAvatarURL($avatar);
-            
+            $msg->setAvatarURL($avatar);  
         }
         $msg->setContent($message);
         $webHook->send($msg);
