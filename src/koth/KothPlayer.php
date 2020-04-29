50510b72a3293fb9bf9f73f3fda3745b86706a17
iZeaoGamer <jameshayward14@icloud.com>
2020-04-29 19:09:17 +0100

    Bug fixes (Untested)

--- src/koth/KothPlayer.php
+++ src/koth/KothPlayer.php
@@ -7,10 +7,9 @@ class KothPlayer{
 public $kothingame = [];
 
     public function isInGame(Player $player): bool{
-        if(isset($this->kothingame[strtolower($player->getName())])){
-	    return $this->kothingame[strtolower($player->getName())];
+
+	    return (isset($this->kothingame[strtolower($player->getName())]) ? $this->kothingame[strtolower($player->getName())] : false);
     }
-}
     public function setInGame(Player $player, bool $kothingame){
         $this->kothingame[strtolower($player->getName())] = $kothingame;
     }
