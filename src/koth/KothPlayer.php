98507d853ca4171b791d60e96925e02aaa7c767a
iZeaoGamer <jameshayward14@icloud.com>
2020-04-29 19:05:09 +0100

    Fixed undex issue

--- src/koth/KothPlayer.php
+++ src/koth/KothPlayer.php
@@ -7,8 +7,10 @@ class KothPlayer{
 public $kothingame = [];
 
     public function isInGame(Player $player): bool{
+        if(isset($this->kothingame[strtolower($player->getName())])){
 	    return $this->kothingame[strtolower($player->getName())];
     }
+}
     public function setInGame(Player $player, bool $kothingame){
         $this->kothingame[strtolower($player->getName())] = $kothingame;
     }
